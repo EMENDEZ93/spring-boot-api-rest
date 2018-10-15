c36d7ffdb7a7f01e4537e2123911b5c5c9d84586
- Edwin Mendez - <emendez@unac.edu.co>
2018-10-15 17:57:03 -0500

    Api Rest

---
+++ src/main/java/em/rest/backend/SpringBootApiRestApplication.java
@@ -0,0 +1,12 @@
+package em.rest.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiRestApplication.class, args);
+	}
+}
