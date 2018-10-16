352e148d06337e6641b9a059374aae1c3217226e
- Edwin Mendez - <emendez@unac.edu.co>
2018-10-15 22:20:05 -0500

    Entity-Model Note

---
+++ src/main/java/em/rest/backend/entity/note/NoteEntity.java
@@ -0,0 +1,62 @@
+package em.rest.backend.entity.note;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class NoteEntity {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	private String name;
+
+	private String title;
+
+	private String description;
+
+	public NoteEntity() {}
+	
+	public NoteEntity(String name, String title, String description) {
+		this.name = name;
+		this.title = title;
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
